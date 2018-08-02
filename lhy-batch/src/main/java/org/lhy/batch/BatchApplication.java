6ebc713d20d1069b565e1b4dd65f40ac5fbcb51a
lihangyu <li.hangyu@goodrabbit.cn>
2018-08-02 16:52:26 +0800

    spring shiro aop transaction

---
+++ lhy-batch/src/main/java/org/lhy/batch/BatchApplication.java
@@ -0,0 +1,15 @@
+package org.lhy.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lihangyu
+ */
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+}
