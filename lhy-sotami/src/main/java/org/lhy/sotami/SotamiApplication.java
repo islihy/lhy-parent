c335f0f8595c84b2718d440771630b3c8719157e
lihangyu <li.hangyu@goodrabbit.cn>
2018-12-19 19:25:29 +0800

    init

---
+++ lhy-sotami/src/main/java/org/lhy/sotami/SotamiApplication.java
@@ -0,0 +1,14 @@
+package org.lhy.sotami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotamiApplication.class, args);
+	}
+
+}
+
